ea8e92e6c7582b5e2bd8c2a6418fcbfc28ffd36e
edwin24o <edwinolivares24@gmail.com>
2024-07-16 19:40:32 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+# 1st question
+
+# Task one
+
+weather = "sunny"
+
+if weather == "sunny":
+   print("Wear sunglasses!")  # shifted to the right is apart of the if block
+else:
+   print("Take an umbrella!") # shifted to the right part of the else code
+
+print('='*50)
+
+# Task two
+
+mood = input('How do you feel today? ')
+
+if mood == 'happy':
+    print("That's great to hear!")
+elif mood == 'sad':
+    print("I hope your day gets better!")
+else:
+    print("Thank you for sharing your feelings!")
+
+print('='*50)
+
+# 2nd question
+
+# Task one
+
